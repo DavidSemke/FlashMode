84f0a0ef223672ce354630fd25d355fc2e33c341
David <david@semke.ca>
2024-10-12 01:07:00 -0700

    Created basic dashboard index view

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from . import views
+from .views import index
 
 
 app_name = "dashboard"
 urlpatterns = [
-    path("", views.IndexView.as_view(), name="index"),
+    path("", index),
+    # path("", views.IndexView.as_view(), name="index"),
     # path("<int:pk>/", views.DetailView.as_view(), name="detail"),
     # path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),
     # path("<int:question_id>/vote/", views.vote, name="vote"),
