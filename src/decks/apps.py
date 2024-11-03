9e63ef13eb73daa290cb14199301e7d2f5e2afd1
David Semke <david@semke.ca>
2024-11-02 21:31:46 -0700

    Refactor(app): Renamed deck_manager app to decks

--- src/decks/apps.py
+++ src/decks/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class DeckManagerConfig(AppConfig):
+class DecksConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "deck_manager"
+    name = "decks"
