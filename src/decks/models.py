1738fb6cfd401b14ae41768864e314948b6296b5
David Semke <david@semke.ca>
2024-11-04 23:42:33 -0800

    Misc(migrations): Pushed migrations for all apps

--- src/decks/models.py
+++ src/decks/models.py
@@ -1,3 +1,20 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Deck(models.Model):
+    users = models.ManyToManyField(
+        settings.AUTH_USER_MODEL, related_name="decks_in_use"
+    )
+    creator = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="decks_created"
+    )
+    create_date = models.DateField(auto_now_add=True)
+    title = models.CharField(max_length=50)
+    private = models.BooleanField(default=False)
+
+
+class Card(models.Model):
+    deck = models.ForeignKey(Deck, on_delete=models.CASCADE, related_name="cards")
+    question = models.CharField(max_length=300)
+    answer = models.CharField(max_length=300)
--- src/study_sessions/models.py
+++ src/study_sessions/models.py
@@ -1,3 +1,19 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+from decks.models import Card
+
+
+class StudySession(models.Model):
+    student = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="study_sessions"
+    )
+    create_date = models.DateField(auto_now_add=True)
+
+
+class Response(models.Model):
+    study_session = models.ForeignKey(
+        StudySession, on_delete=models.CASCADE, related_name="responses"
+    )
+    card = models.ForeignKey(Card, on_delete=models.CASCADE, related_name="responses")
+    is_correct = models.BooleanField()
