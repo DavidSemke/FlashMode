4ccc2cae7a1e0007b91ba2f24c404870bcf76a77
David Semke <david@semke.ca>
2024-11-08 13:12:45 -0800

    Refactor(views): Removed dead code

--- src/flash_mode/views.py
+++ src/flash_mode/views.py
@@ -1,14 +1,6 @@
 from django.shortcuts import render
 from django.urls import reverse
 
-# from allauth.account.views import LoginView
-
 
 def index(request):
     return render(request, "nonoverrides/index.html")
-
-
-# class CustomLoginView(LoginView):
-#     def get_success_url(self):
-#         username = self.request.user.username
-#         return reverse('dashboard:index', args=(username,))
