d84021724440f501d883ea245955b90bb9823d59
David Semke <david@semke.ca>
2024-11-09 22:34:36 -0800

    Misc(settings): Added slippers as template builtin

--- src/flash_mode/settings.py
+++ src/flash_mode/settings.py
@@ -112,6 +112,7 @@ TEMPLATES = [
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
             ],
+            "builtins": ["slippers.templatetags.slippers"],
         },
     },
 ]
