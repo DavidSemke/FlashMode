8c6d26f52615e98f09842a041a8e5445f64eca9f
David Semke <david@semke.ca>
2024-11-29 18:26:27 -0800

    Feat(templates): Deck list page shows ordered, styled list items in a paginated format

--- src/apps/decks/views.py
+++ src/apps/decks/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.db.models import Count
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
 
@@ -7,14 +7,28 @@ from .models import Deck
 
 class DeckListView(ListView):
     model = Deck
-    paginate_by = 20
+    paginate_by = 10
 
-    def get(self, request):
-        return render(request, "decks/deck_list.html", {"head_title": "FlashMode"})
+    def get_queryset(self):
+        return (
+            super()
+            .get_queryset()
+            .select_related("creator")
+            .annotate(card_count=Count("cards"))
+            .order_by("-create_date")
+        )
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["head_title"] = "Search Decks - FlashMode"
+        return context
 
 
 class DeckDetailView(DetailView):
     model = Deck
 
-    def get(self, request):
-        return render(request, "decks/deck_detail.html", {"head_title": "FlashMode"})
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        deck: Deck = self.get_object()
+        context["head_title"] = deck.title + " - FlashMode"
+        return context
