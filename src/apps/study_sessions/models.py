b2819bd600eba2d5a1063936e3c003ea743d1d43
David Semke <david@semke.ca>
2024-12-02 20:50:43 -0800

    Migrate(study_sessions): StudySession now references deck, Response card becomes null on card delete

--- src/apps/study_sessions/models.py
+++ src/apps/study_sessions/models.py
@@ -1,12 +1,15 @@
 from django.contrib.auth.models import User
 from django.db import models
 
-from ..decks.models import Card
+from ..decks.models import Card, Deck
 
 
 class StudySession(models.Model):
     student = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="study_sessions"
+    )
+    deck = models.ForeignKey(
+        Deck, null=True, on_delete=models.SET_NULL, related_name="study_sessions"
     )
     create_date = models.DateField(auto_now_add=True)
 
@@ -15,5 +18,7 @@ class Response(models.Model):
     study_session = models.ForeignKey(
         StudySession, on_delete=models.CASCADE, related_name="responses"
     )
-    card = models.ForeignKey(Card, on_delete=models.CASCADE, related_name="responses")
+    card = models.ForeignKey(
+        Card, null=True, on_delete=models.SET_NULL, related_name="responses"
+    )
     is_correct = models.BooleanField()
