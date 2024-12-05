486fa67db84fa57a0a3481bc8023999b9216f531
David Semke <david@semke.ca>
2024-12-05 00:57:12 -0800

    Refactor(decks): Updated urls.py

--- src/apps/decks/urls.py
+++ src/apps/decks/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
-from .views import DeckDetailView, DeckListView
+from .views.deck_detail import DeckDetailView
+from .views.deck_list import DeckListView
 
 app_name = "decks"
 urlpatterns = [
