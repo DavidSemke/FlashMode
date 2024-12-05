c5098ee50d9fc2213e23c67c4240eeb4b38c1c5f
David Semke <david@semke.ca>
2024-12-05 00:56:21 -0800

    Refactor(decks): Moved deck detailview and listview into separate files, created empty CUD view files

---
+++ src/apps/decks/views/deck_list.py
@@ -0,0 +1,30 @@
+from django.db.models import Count
+from django.views.generic.list import ListView
+
+from ..models import Deck
+
+
+class DeckListView(ListView):
+    model = Deck
+    paginate_by = 10
+
+    def get_queryset(self):
+        queryset = (
+            super()
+            .get_queryset()
+            .select_related("creator")
+            .annotate(card_count=Count("cards"))
+            .order_by("-create_date")
+        )
+
+        query = self.request.GET.get("query")
+
+        if query:
+            queryset = queryset.filter(title__icontains=query)
+
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["head_title"] = "Search Decks - FlashMode"
+        return context
