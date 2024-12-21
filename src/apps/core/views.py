faf940934e12c91e442a4167fcb52990d94c4ea6
David Semke <david@semke.ca>
2024-12-21 13:47:45 -0800

    Refactor(core): IndexView now subclasses TemplateView

--- src/apps/core/views.py
+++ src/apps/core/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
-from django.views import View
+from django.views.generic import TemplateView
 
 
-class IndexView(View):
-    def get(self, request):
-        return render(request, "core/index.html", {"head_title": "FlashMode"})
+class IndexView(TemplateView):
+    template_name = "core/index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["head_title"] = "FlashMode"
+        return context
