3adaaab110a3288d7de4bf4b6981343ef7ac8a04
David Semke <david@semke.ca>
2024-12-27 14:03:39 -0800

    Test(decks): Added passing deck_list tests

---
+++ src/apps/decks/tests/views/test_deck_list.py
@@ -0,0 +1,106 @@
+from django.conf import settings
+from django.test import TestCase
+from django.urls import reverse_lazy
+
+from ....core.model_factories import UserFactory
+from ...model_factories import DeckFactory
+from ...models import Deck
+
+
+class DeckListViewTest(TestCase):
+    url = reverse_lazy("decks:deck_list")
+
+    def setUp(self):
+        self.user1 = UserFactory()
+        self.user2 = UserFactory()
+
+        DeckFactory(creator=self.user1, title="I am a title 1")
+        DeckFactory(creator=self.user1, title="I am a title 2", private=True)
+        DeckFactory(creator=self.user2, title="I am a title 3")
+
+        return super().setUp()
+
+    def test_get(self):
+        with self.assertTemplateUsed("decks/deck_list.html"):
+            res = self.client.get(self.url)
+            self.assertEqual(res.status_code, 200)
+            self.assertIn("head_title", res.context)
+            self.assertIn("main_h1", res.context)
+            self.assertIn("deck_list", res.context)
+
+            deck_list = res.context["deck_list"]
+
+            for deck in deck_list:
+                self.assertIsInstance(deck, Deck)
+
+            # User is anonymous, so only private deck is hidden
+            self.assertEqual(len(deck_list), 2)
+
+    def test_get_login_private_creator(self):
+        # Log in creator of the private deck
+        logged_in = self.client.login(username=self.user1.username, password="password")
+        self.assertTrue(logged_in, "Login failed")
+
+        res = self.client.get(self.url)
+        self.assertEqual(res.status_code, 200)
+
+        deck_list = res.context["deck_list"]
+        self.assertEqual(len(deck_list), 3)
+
+    def test_get_search_query_param(self):
+        res = self.client.get(self.url, query_params={"search": "1"})
+        self.assertEqual(res.status_code, 200)
+
+        deck_list = res.context["deck_list"]
+        self.assertEqual(len(deck_list), 1)
+
+        res = self.client.get(self.url, query_params={"search": "title"})
+        self.assertEqual(res.status_code, 200)
+
+        deck_list = res.context["deck_list"]
+        self.assertEqual(len(deck_list), 2)
+
+    def test_get_creator_id_query_param(self):
+        res = self.client.get(self.url, query_params={"creator_id": self.user1.id})
+        self.assertEqual(res.status_code, 200)
+
+        deck_list = res.context["deck_list"]
+        self.assertEqual(len(deck_list), 1)
+        self.assertEqual(deck_list[0].creator.id, self.user1.id)
+
+    def test_get_guest_collected_query_param(self):
+        # Param collected only applied when logged in
+        # Expect redirect to login page
+        res = self.client.get(self.url, query_params={"collected": "true"})
+        self.assertEqual(res.status_code, 302)
+        self.assertIn(settings.LOGIN_URL, res["Location"])
+
+    # Collected decks result in quick deck access for user
+    def test_get_login_collected_query_param(self):
+        logged_in = self.client.login(username=self.user1.username, password="password")
+        self.assertTrue(logged_in, "Login failed")
+
+        # Decks created by user are considered collected
+        res = self.client.get(self.url, query_params={"collected": "true"})
+        self.assertEqual(res.status_code, 200)
+
+        deck_list = res.context["deck_list"]
+        self.assertEqual(len(deck_list), 2)
+
+        # List all decks NOT collected by user
+        res = self.client.get(self.url, query_params={"collected": "false"})
+        self.assertEqual(res.status_code, 200)
+
+        deck_list = res.context["deck_list"]
+        self.assertEqual(len(deck_list), 1)
+
+        # Make user collect deck not made by them
+        foreign_deck = Deck.objects.get(title="I am a title 3")
+        foreign_deck.users.add(self.user1)
+
+        # List all decks collected by user
+        res = self.client.get(self.url, query_params={"collected": "true"})
+        self.assertEqual(res.status_code, 200)
+
+        deck_list = res.context["deck_list"]
+        self.assertEqual(len(deck_list), 3)
--- src/apps/decks/views/deck_list.py
+++ src/apps/decks/views/deck_list.py
@@ -1,4 +1,6 @@
-from django.db.models import Count
+from django.conf import settings
+from django.db.models import Count, Q
+from django.shortcuts import redirect
 from django.views.generic.list import ListView
 
 from ..models import Deck
@@ -8,19 +10,50 @@ class DeckListView(ListView):
     model = Deck
     paginate_by = 10
 
+    def get(self, request, *args, **kwargs):
+        # The collected url param only applies to login users
+        # Therefore, ask for login if used by anon user
+        collected_param = request.GET.get("collected")
+
+        if collected_param and not request.user.is_authenticated:
+            return redirect(settings.LOGIN_URL)
+
+        return super().get(request, *args, **kwargs)
+
     def get_queryset(self):
         queryset = (
             super()
             .get_queryset()
+            .filter(
+                (Q(private=True) & Q(creator=self.request.user.id)) | Q(private=False)
+            )
             .select_related("creator")
             .annotate(card_count=Count("cards"))
             .order_by("-create_date")
         )
 
-        search = self.request.GET.get("search")
+        search_param = self.request.GET.get("search")
 
-        if search:
-            queryset = queryset.filter(title__icontains=search)
+        if search_param:
+            queryset = queryset.filter(title__icontains=search_param)
+
+        creator_id_param = self.request.GET.get("creator_id")
+
+        if creator_id_param and creator_id_param.isdigit():
+            creator_id = int(creator_id_param)
+            queryset = queryset.filter(creator=creator_id)
+
+        # If collected_param defined here, user is logged in
+        collected_param = self.request.GET.get("collected")
+
+        if collected_param == "true":
+            queryset = queryset.filter(
+                Q(creator=self.request.user.id) | Q(users=self.request.user.id)
+            )
+        elif collected_param == "false":
+            queryset = queryset.exclude(
+                Q(creator=self.request.user.id) | Q(users=self.request.user.id)
+            )
 
         return queryset
 
