68128f48004ffeb32d72805cf9806b4363ae7052
David Semke <david@semke.ca>
2025-01-01 14:49:00 -0800

    Feat(decks): Guests can play decks

--- src/apps/decks/views/deck_play.py
+++ src/apps/decks/views/deck_play.py
@@ -1,26 +1,49 @@
+from random import shuffle
+
+from django.db import transaction
+from django.db.models import Prefetch
+from django.shortcuts import get_object_or_404
 from django.urls import reverse
 from django.views.generic import RedirectView
 
-from ...study_sessions.models import StudySession
+from ...decks.models import Card, Deck
+from ...study_sessions.models import Response, StudySession
 
 
 class DeckPlayView(RedirectView):
     def get(self, request, *args, **kwargs):
-        # if request.user.is_authenticated:
-        #     try:
-        #         self.study_session = StudySession.objects.create(
-        #             student=request.user, deck=kwargs.get("deck_id")
-        #         )
-        #     except:
-        #         pass
+        if request.user.is_authenticated:
+            deck = get_object_or_404(
+                Deck.objects.prefetch_related(
+                    Prefetch("cards", queryset=Card.objects.only("id"))
+                ),
+                pk=kwargs.get("deck_id"),
+            )
+
+            with transaction.atomic():
+                self.study_session = StudySession.objects.create(
+                    student=request.user, deck=deck
+                )
+
+                # deck.cards is a list of card ids
+                card_list = list(deck.cards.all())
+                shuffle(card_list)
+
+                # Method bulk_create is not used because it skips
+                # the save() method
+                # The Response model uses save() for validation
+                for pos, card in enumerate(card_list):
+                    Response.objects.create(
+                        study_session=self.study_session, card=card, position=pos
+                    )
 
         return super().get(request, *args, **kwargs)
 
     def get_redirect_url(self, *args, **kwargs):
-        if self.study_session:
+        if hasattr(self, "study_session"):
             return reverse(
-                "decks:study_session",
-                kwargs={**kwargs, "study_session_id": self.study_session.id},
+                "study_sessions:study_session",
+                kwargs={"study_session_id": self.study_session.id},
             )
         else:
-            return reverse("decks:study_session_guest", kwargs)
+            return reverse("decks:study_session_guest", kwargs={**kwargs})
--- src/apps/decks/views/study_session_guest.py
+++ src/apps/decks/views/study_session_guest.py
@@ -1,20 +1,22 @@
 from django.shortcuts import get_object_or_404
 from django.views.generic import TemplateView
 
-from ..models import Card, Deck
+from ..models import Deck
 
 
 class StudySessionGuestView(TemplateView):
-    template_name = "study_session"
+    template_name = "decks/study_session_guest.html"
 
     def get(self, request, *args, **kwargs):
-        self.deck = get_object_or_404(Deck.objects.get(pk=kwargs.get("deck_id")))
-
+        self.deck = get_object_or_404(
+            Deck.objects.prefetch_related("cards"), pk=kwargs.get("deck_id")
+        )
         return super().get(request, *args, **kwargs)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["deck"] = self.deck
+        context["card_list"] = list(self.deck.cards.values())
         context["main_h1"] = f"Study Session - Deck '{context["deck"].title}'"
         context["head_title"] = f"{context['main_h1']} - FlashMode"
         return context
