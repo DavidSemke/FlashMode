d2b3c2a49e91f3b40591fae3e89bec2142f41a64
David Semke <david@semke.ca>
2025-05-08 00:28:46 -0700

    Fix(dashboard, core): Automate user profile creation with post_save signal

--- src/apps/core/model_factories.py
+++ src/apps/core/model_factories.py
@@ -1,12 +1,16 @@
 from django.contrib.auth import get_user_model
+from django.db.models.signals import post_save
 from factory import Faker, RelatedFactory
-from factory.django import DjangoModelFactory, Password
+from factory.django import DjangoModelFactory, Password, mute_signals
 
 from ..dashboard.model_factories import ProfileFactory
 
 User = get_user_model()
 
 
+# The post_save signal triggers profile creation
+# Ignore the signal here to allow use of ProfileFactory
+@mute_signals(post_save)
 class UserFactory(DjangoModelFactory):
     class Meta:
         model = User
--- src/apps/dashboard/models.py
+++ src/apps/dashboard/models.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.db import models
+from django.db.models.signals import post_save
 
 
 class Profile(models.Model):
@@ -8,3 +9,13 @@ class Profile(models.Model):
     )
     streak = models.PositiveSmallIntegerField(default=0)
     weekly_card_count_goal = models.PositiveSmallIntegerField(default=0)
+
+
+# Use signal to create profile when user created
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        profile = Profile(user=instance)
+        profile.save()
+
+
+post_save.connect(create_profile, sender=settings.AUTH_USER_MODEL)
